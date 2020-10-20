175fafb7173f00eca8e171b9cbeac67c4c92e928
Qijiang Fan <fqj@chromium.org>
2020-10-20 16:11:33 +0900

    Add presubmit check for deprecated libchrome functions.
    BUG=None
    TEST=(manually repo upload in platform2)
    
    Change-Id: Id132c52104362321d3d00c07fba22462a308e299
    Reviewed-on: https://chromium-review.googlesource.com/c/aosp/platform/external/libchrome/+/2486723
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Qijiang Fan <fqj@google.com>
    Tested-by: Qijiang Fan <fqj@google.com>

---
+++ libchrome_tools/check-libchrome.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""
+Utility to check if deprecated libchrome calls are introduced.
+"""
+
+import os
+import re
+import subprocess
+import sys
+
+
+# BAD_KEYWORDS are mapping from bad_keyword_in_regex to
+# error_msg_if_match_found.
+BAD_KEYWORDS = {
+    b'base::MessageLoop::TYPE_':
+    'Use base::MessagePumpType instead',
+    b'base::MessageLoop(?!Current)':
+    'Use base SingleThreadTaskExecutor instead',
+    b'base/message_loop/message_loop.h':
+    'Use base SingleThreadTaskExecutor instead',
+}
+
+
+def main():
+    files = os.environ['PRESUBMIT_FILES'].split('\n')
+
+    errors = []
+
+    for f in files:
+        if not (f.endswith('.h') or f.endswith('.cc')):
+            continue
+        diff = subprocess.check_output(
+            ['git', 'show', '--oneline', os.environ['PRESUBMIT_COMMIT'],
+             f]).split(b'\n')
+        for line in diff:
+            if not line.startswith(b'+'):
+                continue
+            for bad_pattern, error_message in BAD_KEYWORDS.items():
+                m = re.search(bad_pattern, line)
+                if m:
+                    errors.append('In File %s, found %s (pattern: %s), %s' %
+                                  (f, m.group(0), bad_pattern.decode('ascii'),
+                                   error_message))
+                    break
+
+    if errors:
+        print('\n'.join(errors), file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
