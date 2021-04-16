a6e04bf82244f9a04ccf74dce021123a478cc194
Qijiang Fan <fqj@chromium.org>
2021-04-16 14:12:03 +0900

    update_upstream.py to support add additional files.
    BUG=b:180558819
    TEST=./update_upstream.py cros/upstream chromium-tot  --all
    
    Change-Id: If490f440d1507a6f52fa05a39033ce1c3fe0c8fa
    Reviewed-on: https://chromium-review.googlesource.com/c/aosp/platform/external/libchrome/+/2827447
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Qijiang Fan <fqj@google.com>
    Tested-by: Qijiang Fan <fqj@google.com>

--- libchrome_tools/developer-tools/uprev/update_upstream.py
+++ libchrome_tools/developer-tools/uprev/update_upstream.py
@@ -5,24 +5,51 @@
 """
 Script to generate cros/upstream branch.
 
-Usage: update_upstream.py [--delete | --forward | --all]
+Usage: update_upstream.py [--delete | --add | --forward | --all]
 
 --delete: Run delete stage to delete unnecessary files. Create one single commit
 to delete unnecessary files.
+--add: Run add stage to add new files. Create a series of commits containing the
+history of new files, and one merge commit to add to cros/upstream.
 --forward: Run forward stage to update to newer version. Create a series commits
 to update cros/upstream to newer revision.
---all: Run all stages. equals to --delete --forward
+--all: Run all stages. equals to --delete --add --forward
 """
 
 import argparse
 import subprocess
 import sys
+import tempfile
 
 import filter_config
 import filtered_utils
 import filters
 import lazytree
 import utils
+
+GENERATED_FILTERED_TREE = 'libchrome_tools/developer-tools/uprev/generate_filtered_tree.py'
+CROS_LIBCHROME_RECOVERED_FROM_COMMIT = b'CrOS-Libchrome-Recovered-From-Commit'
+INITIAL_COMMIT = 'ba8bd83211d4bbca7a48793d567b06d5d4451005'
+
+
+def verify(current):
+    """ Verifies current commit has the filtered files matching filter_config.
+
+    Returns True if if's correct, False otherwise.
+
+    Args:
+        current: current cros/upstream commit hash.
+    """
+    current_metadata = filtered_utils.get_metadata(current)
+    original_commit = current_metadata.original_commit_cursor
+    new_filter = filters.Filter(filter_config.WANT, filter_config.WANT_EXCLUDE,
+                                filter_config.KEEP, filter_config.KEEP_EXCLUDE)
+    expected_tree = lazytree.LazyTree()
+    for f in utils.get_file_list(original_commit):
+        if new_filter.want_file(f.path):
+            expected_tree[f.path] = f
+
+    return expected_tree.hash() == current_metadata.tree
 
 
 def delete(current):
@@ -55,6 +82,66 @@ def delete(current):
     return new_commit.decode('ascii')
 
 
+def add(current):
+    """ Adds newly wanted files for new filters.
+
+    Args:
+        current: current cros/upstream commit hash.
+    """
+    current_metadata = filtered_utils.get_metadata(current)
+    original_commit = current_metadata.original_commit_cursor
+    new_filter = filters.Filter(filter_config.WANT, filter_config.WANT_EXCLUDE,
+                                filter_config.KEEP, filter_config.KEEP_EXCLUDE)
+
+    tree = lazytree.LazyTree()
+
+    original_files = utils.get_file_list(original_commit)
+    new_files = set(new_filter.filter_files([], original_files))
+    for f in new_files:
+        if new_filter.want_file(f.path):
+            tree[f.path] = f
+
+    if tree.hash() == current_metadata.tree:
+        return current
+
+    old_files = set(utils.get_file_list(current))
+    files_to_add = list(new_files.difference(old_files))
+
+    if not files_to_add:
+        return current
+
+    proc = subprocess.run([
+        GENERATED_FILTERED_TREE,
+        INITIAL_COMMIT,
+        original_commit,
+        '--verbose',
+        '--commit_hash_meta_name',
+        CROS_LIBCHROME_RECOVERED_FROM_COMMIT,
+        '--filter_files',
+        '/dev/stdin',
+    ],
+                          input=b''.join(f.path + b'\n' for f in files_to_add),
+                          stdout=subprocess.PIPE)
+    assert proc.returncode == 0
+    last_commit = proc.stdout.strip().decode('ascii')
+
+    current_tree = lazytree.LazyTree(current_metadata.tree)
+    new_file_history_head_filelist = utils.get_file_list(last_commit)
+    for f in new_file_history_head_filelist:
+        current_tree[f.path] = f
+
+    assert tree.hash() == current_tree.hash(), (tree.hash(),
+                                                current_tree.hash())
+
+    msg = b'Add new files due to filter change\n\n%s: %s' % (
+        filtered_utils.CROS_LIBCHROME_CURRENT_COMMIT, original_commit)
+    new_commit = subprocess.check_output(
+        ['git', 'commit-tree', '-p', current, '-p', last_commit,
+         tree.hash()],
+        input=msg).strip()
+    return new_commit.decode('ascii')
+
+
 def forward(current, target):
     """ Forwards to given upstream Chromium commit.
 
@@ -65,7 +152,7 @@ def forward(current, target):
         target: target commit in Chromium src tree.
     """
     proc = subprocess.run([
-        'libchrome_tools/developer-tools/uprev/generate_filtered_tree.py',
+        GENERATED_FILTERED_TREE,
         current,
         target,
         '--verbose',
@@ -102,6 +189,12 @@ def main():
                         const=True,
                         default=False,
                         help='Run delete files stage.')
+    parser.add_argument('--add',
+                        dest='add',
+                        action='store_const',
+                        const=True,
+                        default=False,
+                        help='Run add files stage.')
     parser.add_argument('--forward',
                         dest='forward',
                         action='store_const',
@@ -114,12 +207,17 @@ def main():
     current = args.current
     target = args.target
 
-    assert args.all or args.delete or args.forward
+    assert args.all or args.delete or args.add or args.forward
 
     if args.all or args.delete:
         current = delete(current)
 
+    if args.all or args.add:
+        current = add(current)
+
     if args.all or args.forward:
+        assert verify(
+            current), 'Files must be correctly filtered before forward stage'
         current = forward(current, target)
 
     print(current)
